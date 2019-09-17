7d59908f2c93b77b080e288e5213eb735df3310f
htljoyj <htljoyj@gmail.com>
2019-09-17 10:59:16 +0200

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+let number = 7;
+
+if(number < 6)
+    console.log('onvoldoende');
+else if (number > 6 && number <= 7)
+    console.log('voldoende');
+else if(number > 7 && number <= 9)
+    console.log('goed');
+else if(number > 9)
+    console.log('uitmuntend');
